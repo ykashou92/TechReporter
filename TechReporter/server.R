58134e1f63adc28d4316ffc6d0ef7f787707a035
Yanal Kashou <yanal.kashou@gmail.com>
2016-10-09 13:03:04 +0300

    First exploration, implemented submitbutton that generates a plot.

--- TechReporter/server.R
+++ TechReporter/server.R
@@ -8,30 +8,31 @@
 #
 
 library(shiny)
-getSymbols("AAPL")
-chartSeries(GOOG, subset = 'last 6 months', theme = chartTheme("white"))
-addSMA(n = 200, col = "red")
-addSMA(n = 50, col = "blue")
-addSMA(n = 20, col = "yellow") 
-addSMI()
-addRSI()
-addMACD()
-
-
-
+library(quantmod)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
   
+  # Make a chart for a symbol, with the settings from the inputs
+  stock.sym <- reactive ({
+    tryCatch({
+      suppressWarnings(getSymbols(input$stock, auto.assign = FALSE))
+    }, error = function(err) {
+      return(NULL)
+      })
+  })
+
+  output$mainPlot <- renderPlot({
+    if(!is.null(stock.sym())) {
+    chartSeries(stock.sym(),
+                name = input$stock,
+                subset = 'last 6 months', 
+                theme = chartTheme("white", 
+                                   up.col = "#87CEEB", 
+                                   dn.col = "red", 
+                                   fg.col = "black", 
+                                   bg.col = "white"))
+    }
+  })
 })
+  
--- TechReporter/ui.R
+++ TechReporter/ui.R
@@ -13,21 +13,18 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("TechReporter"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+       textInput("stock", "Symbol:", value = "^DJI"),
+       submitButton(text = "Generate Report")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       plotOutput("mainPlot")
     )
   )
 ))
